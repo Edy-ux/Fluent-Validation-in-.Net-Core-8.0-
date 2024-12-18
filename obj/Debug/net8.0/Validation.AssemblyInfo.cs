eccdc760ed0ec98f066c19290fe1dd4da827ed8f
Ednei <fenderlopes@gmail.com>
2024-12-18 18:12:20 -0300

    added gitignore

--- obj/Debug/net8.0/Validation.AssemblyInfo.cs
+++ obj/Debug/net8.0/Validation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Validation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e86e7ec6a787e73cbb5f0eda72dc8eca72d14b03")]
 [assembly: System.Reflection.AssemblyProductAttribute("Validation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Validation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
